1a30ebfe06114e5da71f84052682ba7c084fa348
noorzeyad <110266242+noorzeyad@users.noreply.github.com>
2022-08-20 09:31:05 +0300

    cw7 is done
    done

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 
+ function BMI(weight,height) {
+ let result =   weight / (height * height);
+    return result;
+ }
+
+ console.log(BMI(50,1.6)); 
+
+  function status(bmi) {
+if (bmi<18.5){ 
+   return "لديك نقص في الوزن"; 
+} else if (bmi >= 18.5 && bmi < 25) {
+   return "وزنك صحي"
+} else {
+   return "لديك زيادة في الوزن"   
+}
+  }
+
+  function calculate () {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+
+   let bmi = BMI(weight, height);
+   let desc = status(bmi);
+   let div = decument.getElementById("result");
+   div.ineerText = bmi + " == " + desc
+   
+}
